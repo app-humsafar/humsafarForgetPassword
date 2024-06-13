f010529ae1162f24e2f8be84d622468f2632670f
umer-shehzad <umer.work.96@gmail.com>
2024-06-13 18:11:58 +0500

    update the route

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/reset-password/:token" element={<MainComponent />} />
+        <Route path="/reset-password" element={<MainComponent />} />
         {/* Add other routes here */}
       </Routes>
     </Router>
