99413ac8e7d8241aba3f1e8846924fcc89df86c1
umer-shehzad <umer.work.96@gmail.com>
2024-06-13 19:12:14 +0500

    add success component

--- src/components/ResetPasswordForm.jsx
+++ src/components/ResetPasswordForm.jsx
@@ -1,13 +1,18 @@
 // src/ResetPasswordForm.js
 import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
 import { TextField, Button, Typography, Container, Box } from '@mui/material';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline'; // Import the check icon
+import { useLocation } from 'react-router-dom';
 
 const ResetPasswordForm = () => {
-    const { token } = useParams(); // Extract the token from the URL
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
+    const token = searchParams.get('token');
+
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
     const [message, setMessage] = useState('');
+    const [isSuccess, setIsSuccess] = useState(false); // State to handle success
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -20,8 +25,8 @@ const ResetPasswordForm = () => {
         console.log('Token:', token);
         console.log('Password:', password);
 
-        // Reset the message
-        setMessage('Password has been reset successfully.');
+        // Set success state
+        setIsSuccess(true);
 
         // Reset states
         setPassword('');
@@ -30,125 +35,145 @@ const ResetPasswordForm = () => {
 
     return (
         <Container maxWidth="sm">
-            <Box
-                component="form"
-                onSubmit={handleSubmit}
-                sx={{
-                    mt: 5,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    p: 3,
-                    border: '1px solid #ccc',
-                    borderRadius: 1,
-                    boxShadow: 3
-                }}
-            >
-                <Typography variant="h4" component='h2' color='white' gutterBottom>
-                    Reset Password
-                </Typography>
-
-                <TextField
-                    type="password"
-                    label="New Password"
-                    variant="outlined"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                    InputLabelProps={{
-                        style: {
-                            color: 'white',
-                            '&:hover': {
-                                color: 'white',
-                            },
-                        },
-                    }}
-                    InputProps={{
-                        style: {
-                            color: 'white',
-                            '&:hover': {
-                                color: 'black',
-                            },
-                            '&:focus': {
-                                color: 'black',
-                            },
-                        },
-                    }}
+            {isSuccess ? (
+                <Box
                     sx={{
-                        width: '90%',
-                        mb: 2,
-                        '& .MuiOutlinedInput-root': {
-                            '& fieldset': {
-                                borderColor: 'white',
-                            },
-                            '&:hover fieldset': {
-                                borderColor: 'white',
-                            },
-                            '&.Mui-focused fieldset': {
-                                borderColor: 'white',
-                            },
-                        },
+                        mt: 5,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        p: 3,
+                        border: '1px solid #ccc',
+                        borderRadius: 5,
+                        boxShadow: 3,
                     }}
-                />
-
-                <TextField
-                    type="password"
-                    label="Confirm Password"
-                    variant="outlined"
-                    value={confirmPassword}
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                    required
-                    InputLabelProps={{
-                        style: {
-                            color: 'white',
-                            '&:hover': {
-                                color: 'white',
-                            },
-                        },
-                    }}
-                    InputProps={{
-                        style: {
-                            color: 'white',
-                            '&:hover': {
-                                color: 'black',
-                            },
-                            '&:focus': {
-                                color: 'black',
-                            },
-                        },
-                    }}
-                    sx={{
-                        mb: 2,
-                        width: '90%',
-                        '& .MuiOutlinedInput-root': {
-                            '& fieldset': {
-                                borderColor: 'white',
-                            },
-                            '&:hover fieldset': {
-                                borderColor: 'white',
-                            },
-                            '&.Mui-focused fieldset': {
-                                borderColor: 'white',
-                            },
-                        },
-                    }}
-                />
-
-                <Button
-                    type="submit"
-                    variant="contained"
-                    sx={{ width: '80%' }}
-                    style={{ backgroundColor: 'black', color: 'white' }}
                 >
-                    Reset Password
-                </Button>
-
-                {message && (
-                    <Typography variant="body2" color="textSecondary" align="center" sx={{ mt: 2 }}>
-                        {message}
+                    <CheckCircleOutlineIcon sx={{ color: 'green', fontSize: { lg: 90, xs: 50 } }} />
+                    <Typography variant="h6" color="white" textAlign="center" sx={{ mt: 2 }}>
+                        Your password has been successfully changed.
                     </Typography>
-                )}
-            </Box>
+                </Box>
+            ) : (
+                <Box
+                    component="form"
+                    onSubmit={handleSubmit}
+                    sx={{
+                        mt: 5,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        p: 3,
+                        border: '1px solid #ccc',
+                        borderRadius: 5,
+                        boxShadow: 3,
+                    }}
+                >
+                    <Typography variant="h4" component='h2' color='white' gutterBottom>
+                        Reset Password
+                    </Typography>
+
+                    <TextField
+                        type="password"
+                        label="New Password"
+                        variant="outlined"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                        InputLabelProps={{
+                            style: {
+                                color: 'white',
+                                '&:hover': {
+                                    color: 'white',
+                                },
+                            },
+                        }}
+                        InputProps={{
+                            style: {
+                                color: 'white',
+                                '&:hover': {
+                                    color: 'black',
+                                },
+                                '&:focus': {
+                                    color: 'black',
+                                },
+                            },
+                        }}
+                        sx={{
+                            width: '90%',
+                            mb: 2,
+                            '& .MuiOutlinedInput-root': {
+                                '& fieldset': {
+                                    borderColor: 'white',
+                                },
+                                '&:hover fieldset': {
+                                    borderColor: 'white',
+                                },
+                                '&.Mui-focused fieldset': {
+                                    borderColor: 'white',
+                                },
+                            },
+                        }}
+                    />
+
+                    <TextField
+                        type="password"
+                        label="Confirm Password"
+                        variant="outlined"
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                        required
+                        InputLabelProps={{
+                            style: {
+                                color: 'white',
+                                '&:hover': {
+                                    color: 'white',
+                                },
+                            },
+                        }}
+                        InputProps={{
+                            style: {
+                                color: 'white',
+                                '&:hover': {
+                                    color: 'black',
+                                },
+                                '&:focus': {
+                                    color: 'black',
+                                },
+                            },
+                        }}
+                        sx={{
+                            mb: 2,
+                            width: '90%',
+                            '& .MuiOutlinedInput-root': {
+                                '& fieldset': {
+                                    borderColor: 'white',
+                                },
+                                '&:hover fieldset': {
+                                    borderColor: 'white',
+                                },
+                                '&.Mui-focused fieldset': {
+                                    borderColor: 'white',
+                                },
+                            },
+                        }}
+                    />
+
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        sx={{ width: '80%' }}
+                        style={{ backgroundColor: 'black', color: 'white' }}
+                    >
+                        Reset Password
+                    </Button>
+
+                    {message && (
+                        <Typography variant="body2" color="error" align="center" sx={{ mt: 2 }}>
+                            {message}
+                        </Typography>
+                    )}
+                </Box>
+            )}
         </Container>
     );
 };
