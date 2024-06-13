efa546859b0959eff740c9c166ead52f19a7cb2b
umer-shehzad <umer.work.96@gmail.com>
2024-06-13 17:38:24 +0500

    Add Main Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import ResetPasswordForm from './components/ResetPasswordForm';
+import MainComponent from './components/MainComponent';
+
 
 function App() {
 
   return (
     <Router>
       <Routes>
-        <Route path="/reset-password/:token" element={<ResetPasswordForm />} />
+        <Route path="/reset-password/:token" element={<MainComponent />} />
         {/* Add other routes here */}
       </Routes>
     </Router>
